a045c214faee442d2a322290c4c7f1ed651e3e39
TendyWijaya123 <117370634+TendyWijaya123@users.noreply.github.com>
2023-08-25 05:14:36 +0700

    Add files via upload

---
+++ Tugas1/src/tugas1/Soal1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Soal1 {
+
+    public static void main(String[] args){
+        byte angka1 = 125;
+        byte angka2 = 6;
+        byte hasil = (byte) (angka1+angka2);
+        
+        System.out.println("Hasil 1:"+hasil);
+    }
+    
+}
---
+++ Tugas1/src/tugas1/Soal2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Soal2 {
+
+    public static void main(String[] args) {
+        int i= 42;
+        String s= (i<10)?"life":(i>50)?"universe":"everything;";
+        System.out.println(s);
+    }
+    
+}
